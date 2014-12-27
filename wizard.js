d4e9597a7efa6a983f421f4a09dbd85b7c38ac88
Jascha Ehrenreich <jascha@jaeh.at>
2014-12-27 16:03:57 +0100

    started to add deployment, still refactoring

--- wizard.js
+++ wizard.js
@@ -1,19 +1,35 @@
 #!/usr/bin/env node
 'use strict';
-
-var path = require('path')
-  , fs      = require('fs')
-  , program = require('commander')
-  , cli     = require(path.join(__dirname,'lib') )
+var path     = require('path')
+  , fs       = require('fs')
+  , program  = require('commander')
+  , execFile = require('child_process').execFile
+  , spawn    = require('child_process').spawn
+  , log      = require('magic-log')
+  , nodegit  = require('nodegit')
+  , cli      = require(path.join(__dirname, 'lib') )
 ;
 
-function exec() {
+module.exports = function xc() {
     
   program.version('0.0.1');
 
   if ( process.argv.length <= 2 ) {
     cli.promptForAction();
   }
+  
+  program
+    .command('test')
+    .description('test command')
+    .action(function () {
+      log('nodegit:', nodegit);
+      for (var k in nodegit ) {
+        log(nodegit[k]);
+      }
+      var s = nodegit.Submodule();
+      log(s);
+    })
+  ;
 
   program
     .command('*')
@@ -27,4 +43,3 @@ function exec() {
     console.log('help text');
   }
 }
-module.exports = exec;
